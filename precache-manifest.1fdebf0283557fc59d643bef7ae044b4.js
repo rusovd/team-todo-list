d424a4132875544b26cc9b5841850ea3d91265e5
Denys Rusov <rusovd@gmail.com>
2019-08-20 16:06:13 +0300

    Updates

---
+++ precache-manifest.1fdebf0283557fc59d643bef7ae044b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2301f8a07cde113057c2d56e2fd71ae",
+    "url": "/team-todo-list/index.html"
+  },
+  {
+    "revision": "eaea294fcb72c23e7021",
+    "url": "/team-todo-list/static/js/2.0fc77fb1.chunk.js"
+  },
+  {
+    "revision": "fcf9d3ac96f3f6e62093",
+    "url": "/team-todo-list/static/js/main.caf00346.chunk.js"
+  },
+  {
+    "revision": "5f54b4013103516b3ae9",
+    "url": "/team-todo-list/static/js/runtime~main.c6b6e377.js"
+  },
+  {
+    "revision": "8086623cc49767eb6ef48a5ae0cf73ed",
+    "url": "/team-todo-list/static/media/menu.8086623c.svg"
+  },
+  {
+    "revision": "9b08278346507cef320f9f9556af5c09",
+    "url": "/team-todo-list/static/media/plus.9b082783.svg"
+  },
+  {
+    "revision": "d7bec4e6ace665bae3d9410540a4a0b2",
+    "url": "/team-todo-list/static/media/trash.d7bec4e6.svg"
+  }
+]);
